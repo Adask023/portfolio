70834d47c8c59b4042530425c6b4044f804e9728
unknown <adam.kudlacik@billennium.com>
2022-03-01 16:08:03 +0100

    add sass

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import component from "./component";
+import "./styles/style.scss";
+
+// pobieranie komponentu a następnie dołączanie jako child
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports = {
   entry: {
     main: path.resolve(__dirname, "./src/app.js"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "deploy"),
