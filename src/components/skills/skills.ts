f62374017973dd3c4b2d37315c7bccf4eb5494ad
unknown <adam.kudlacik@billennium.com>
2022-03-08 16:15:52 +0100

    add skills section

--- src/components/skills/skills.ts
+++ src/components/skills/skills.ts
@@ -5,12 +5,35 @@ export const skills = () => {
   const htmlString = require("./skills-static.html");
   const element = loadTemplate(htmlString);
 
-  const skills = element.querySelector(".skills");
+  const generateSkillsSection = () => {
+    // const skills = element.querySelector(".skills");
+    const circleItem1 = element.querySelector(".circle-1");
+    const circleItem1Header = element.querySelector(".circle-1-header") as HTMLElement;
+    const circleItem2 = element.querySelector(".circle-2");
+    const circleItem2Header = element.querySelector(".circle-2-header");
+    const circleItem3 = element.querySelector(".circle-3");
+    const circleItem3Header = element.querySelector(".circle-3-header");
 
-  const loaderInstance = new CreateLoadingCircle();
-  const jsLoader = loaderInstance.create();
+    setTimeout(() => {
+      const loaderInstance = new CreateLoadingCircle(90, 0.2, "blue", "1");
+      const jsLoader = loaderInstance.create();
+      circleItem1.appendChild(jsLoader);
+    }, 1000);
 
-  skills.appendChild(jsLoader);
+    setTimeout(() => {
+      const loaderInstance2 = new CreateLoadingCircle(80, 0.2, "darkpink", "2");
+      const jsLoader2 = loaderInstance2.create();
+      circleItem2.appendChild(jsLoader2);
+    }, 1500);
+
+    setTimeout(() => {
+      const loaderInstance3 = new CreateLoadingCircle(70, 0.2, "yellow", "3");
+      const jsLoader3 = loaderInstance3.create();
+      circleItem3.appendChild(jsLoader3);
+    }, 2000);
+  };
+
+  generateSkillsSection();
 
   return element;
 };
--- src/util/CreateLoadingCircle.ts
+++ src/util/CreateLoadingCircle.ts
@@ -1,25 +1,108 @@
 interface ICreateLoadingCircle {
+  constructor: Function;
   create: () => Node;
+  createSvg: () => Node;
+  pickVariant: () => string;
 }
 
 export class CreateLoadingCircle implements ICreateLoadingCircle {
   private level: number;
   private delay: number;
+  private variant: string;
+  private id: string;
 
-  constructor(level = 10, delay = 0.2) {
+  constructor(level = 50, delay = 0.2, variant = "blue", id: string) {
     this.level = level;
     this.delay = delay;
+    this.variant = variant;
+    this.id = id;
+  }
+
+  pickVariant() {
+    switch (this.variant) {
+      case "blue":
+        return "#aef1ff";
+      case "pink":
+        return "#e8a3ec";
+      case "yellow":
+        return "#ffd79c";
+      case "darkblue":
+        return "#3e3de6";
+      case "darkpink":
+        return "#e7618b";
+      default:
+        return "#aef1ff";
+    }
+  }
+
+  createSvg() {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("height", "160px");
+    svg.setAttribute("width", "160px");
+
+    const svgUrl = "http://www.w3.org/2000/svg";
+    const svgCircle = document.createElementNS(svgUrl, "circle");
+    svgCircle.setAttribute("cx", "80");
+    svgCircle.setAttribute("cy", "80");
+    svgCircle.setAttribute("r", "70");
+    svgCircle.setAttribute("stroke-linecap", "round");
+    svgCircle.setAttribute(
+      "style",
+      `fill: none;
+    stroke: ${this.pickVariant()};
+    stroke-width: 20px;
+    stroke-dasharray: 472;
+    stroke-dashoffset: 472;
+    animation: circleanim${this.id} 2s ease forwards;
+    `
+    );
+
+    let dynamicStyles = null;
+    dynamicStyles = document.createElement("style");
+    document.head.appendChild(dynamicStyles);
+    dynamicStyles.sheet.insertRule(
+      `@keyframes circleanim${this.id} {
+      100% {
+        stroke-dashoffset: ${472 - 472 * (this.level / 100) + 20};
+      }
+    }`
+    );
+
+    svg.appendChild(svgCircle);
+
+    return svg;
   }
 
   create() {
-    console.log(this.level);
-    console.log(this.delay);
-
     const element = document.createElement("div");
-    const h1 = document.createElement("h1");
-    
-    h1.innerHTML = "hello from loader!";
-    element.appendChild(h1);
+    element.classList.add("skill");
+
+    const outer = document.createElement("div");
+    outer.classList.add("outer");
+
+    const inner = document.createElement("div");
+    inner.classList.add("inner");
+
+    const circleNumber = document.createElement("div");
+    circleNumber.classList.add("circle-number");
+
+    // let circleNumber = element.querySelector(".circle-number");
+    let counter = 0;
+    setInterval(() => {
+      if (counter == this.level) {
+        clearInterval();
+      } else {
+        counter += 1;
+        circleNumber.innerHTML = counter + "%";
+      }
+    }, 1000 * (2 / this.level));
+
+    inner.appendChild(circleNumber);
+    outer.appendChild(inner);
+    element.appendChild(outer);
+    const nevSvg = this.createSvg();
+    element.appendChild(nevSvg);
+
     return element;
   }
 }
